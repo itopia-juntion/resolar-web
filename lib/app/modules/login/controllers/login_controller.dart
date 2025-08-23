ab8dbf9ce5bdad2a2977732d86f6e343da64ff1c
jungwuk-ryu <vojougae35@gmail.com>
2025-08-24 01:26:32 +0900

    refactor: 테스트용 코드 제거

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
@@ -15,22 +13,6 @@ class LoginController extends GetxController {
   final RxBool _loggingIn = RxBool(false);
 
   bool get loggingIn => _loggingIn.value;
-
-  @override
-  void onInit() async {
-    super.onInit();
-    var as = Get.find<AuthService>();
-    RequestResult res1 = await as.login('testuser', 'password1232');
-    RequestResult res2 = await as.login('testuser', 'password123');
-
-    if (res1 is RequestFail) {
-      log("성공!0 ");
-    }
-
-    if (res2 is RequestSuccess) {
-      log("성공!");
-    }
-  }
 
   @override
   void onReady() {
