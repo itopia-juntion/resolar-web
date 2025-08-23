d13daf0b3fbd88cdd09aeeab8f1cdb0fc40972b2
jungwuk-ryu <vojougae35@gmail.com>
2025-08-24 04:14:22 +0900

    feat: 주제 선택시 홈 헤더 타이틀 변경

--- lib/app/modules/home/widgets/header.dart
+++ lib/app/modules/home/widgets/header.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:resolar_web/app/modules/home/controllers/home_controller.dart';
 import 'package:resolar_web/app/widgets/app_input.dart';
 
 import '../../../core/constants/app_colors.dart';
@@ -13,9 +15,11 @@ class Header extends StatelessWidget {
     return Row(
       spacing: 16,
       children: [
-        const Text(
-          'All Links',
-          style: TextStyle(fontSize: 34, fontWeight: FontWeight.w800),
+        Obx(
+          () => Text(
+            Get.find<HomeController>().selectedSubject?.name ?? '',
+            style: TextStyle(fontSize: 34, fontWeight: FontWeight.w800),
+          ),
         ),
         Expanded(child: SizedBox()),
         SizedBox(
